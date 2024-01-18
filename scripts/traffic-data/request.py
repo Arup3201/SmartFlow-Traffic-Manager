f569bb3c42ad2466391bf2f38173c76098a72f5a
arupjana7365@gmail.com <arupjana7365@gmail.com>
2024-01-18 21:55:51 +0530

    update script

--- scripts/traffic-data/request.py
+++ scripts/traffic-data/request.py
@@ -1,12 +1,9 @@
 import requests
 
-url = "https://tsaboin-tsaboin-cams-v1.p.rapidapi.com/listall.json"
+url = "https://api.helios.earth/v1/cameras?bbox=-77.636,43.142,-77.582,43.171"
 
-headers = {
-	"X-RapidAPI-Key": "77edeb4b2amsh26bc0277bad393dp117823jsn7451c2ea896e",
-	"X-RapidAPI-Host": "tsaboin-tsaboin-cams-v1.p.rapidapi.com"
-}
 
-response = requests.get(url, headers=headers)
+
+response = requests.get(url)
 
 print(response.json())
